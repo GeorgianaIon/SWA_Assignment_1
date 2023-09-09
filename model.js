f73ce1df8a76ba32d68b137f100e29bf6226a6f2
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-09-09 08:54:22 +0200

    Added Model.js

---
+++ model.js
@@ -0,0 +1,26 @@
+function DataMeasurement(type, time, place, unit) {
+    return { 
+        getType: () => type, 
+        getTime: () => time, 
+        getPlace: () => place, 
+        getUnit: () => unit
+    }
+}
+
+function HistoricalMeasurement({type, time, place, unit, value}){
+    const dataMeasurement = DataMeasurement(type, time, place, unit)
+    return {
+        ... dataMeasurement,
+        getValue: () => value
+    }
+}
+
+function ForecastMeasurement({type, time, place, value, unit, from, to, precipitation_types, directions}) {
+    const measurement = DataMeasurement(type, time, place, value, unit)
+    return {...measurement,
+        getFrom: ()=> from, 
+        getTo: () => to,
+        getPrecipitationTypes: () => precipitation_types,
+        getDirections: () => directions
+    }
+}
