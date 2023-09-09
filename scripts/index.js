c5f34c5f4c61816ac09292828e687fb93abb8c47
Georgiana <ion.georgiana@hotmail.com>
2023-09-09 20:58:12 +0200

    filter cities

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,10 @@ import { AARHUS_ROUTE, COPENHAGEN_ROUTE, HORSENS_ROUTE } from "./constants.js";
 import HttpClient from "./HttpClient.js";
 import { constructCard } from "../generateHtml/construct-card.js";
 import model from "./model.js"
-import { MinTemperature, MaxTemperature, AverageWindSpeed, TotalPrecipitation } from "./model.js"
 
 const { getFetchAsync } = HttpClient();
 const resultsContainer = document.getElementsByClassName("weather-data")[0];
-
+const selectCity = document.getElementById("city-select");
 
 const getWeatherForAllCities = async () => {
     return await Promise.all([
@@ -16,20 +15,41 @@ const getWeatherForAllCities = async () => {
     ]);
 };
 
+
+//all weather data
 const weatherData = await getWeatherForAllCities();
 
-
-weatherData.forEach(element => {
-    const latestMeasurement = model(element).latestMeasurements
-
-    latestMeasurement.forEach(latestElement => {
-        resultsContainer.appendChild(constructCard(latestElement))
-    });
-
+selectCity.addEventListener('change', () => {
+    const selectedCity = selectCity.value;
+    displayWeatherData(selectedCity);
 });
 
+//weather data for each city
 const [horsensWeather, aarhusWeather, copenhagenWeather] =
     await getWeatherForAllCities();
 
-console.log(MaxTemperature(model(horsensWeather).historicalMeasurements))
+const displayWeatherData = (selectedCity) => {
+    resultsContainer.innerHTML = '';
+
+    const selectedWeather = getSelectedWeatherData(selectedCity);
+
+    selectedWeather.forEach(element => {
+        resultsContainer.appendChild(constructCard(element));
+    });
+};
+
+const getSelectedWeatherData = (selectedCity) => {
+    switch (selectedCity) {
+        case 'horsens':
+            return model(horsensWeather).latestMeasurements;
+        case 'aarhus':
+            return model(aarhusWeather).latestMeasurements;
+        case 'copenhagen':
+            return model(copenhagenWeather).latestMeasurements;
+        default:
+            return [];
+    }
+};
+
+displayWeatherData('horsens');
 
