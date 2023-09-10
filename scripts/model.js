5713815a3c9f938acf64cb23addada4b16d9c2e2
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-09-10 12:10:43 +0200

    Update model.js

--- scripts/model.js
+++ scripts/model.js
@@ -9,7 +9,7 @@ function DataMeasurement(type, time, place, unit) {
     }
 }
 
-function HistoricalMeasurement({ type, time, place, unit, value }) {
+function HistoricalMeasurement(type, time, place, unit, value ) {
     const dataMeasurement = DataMeasurement(type, time, place, unit)
     return {
         ...dataMeasurement,
@@ -29,70 +29,81 @@ function ForecastMeasurement({ type, time, place, value, unit, from, to, precipi
 }
 
 const model = (weatherData) => {
-    const historicalMeasurements = weatherData.map(data => HistoricalMeasurement({ type: data.type, time: data.time, place: data.place, unit: data.unit, value: data.value }))
+    const historicalMeasurements = weatherData.map(data => HistoricalMeasurement(data.type, data.time, data.place, data.unit, data.value))
 
     const forecastMeasurements = weatherData.map(data => ForecastMeasurement({
         type: data.type, time: data.time, place: data.place, value: data.value, unit: data.unit, from: data.from,
         to: data.to, precipitation_types: data.precipitation_types, directions: data.directions
     }))
 
-    let latestMeasurements = [];
-    let types = WEATHER_TYPES;
+    const latestMeasurements = LatestMeasurements(historicalMeasurements)
+    const minTemperature = MinTemperature(weatherData)
+    const maxTemperature = MaxTemperature(weatherData)
+    const totalPrecipitation = TotalPrecipitation(weatherData)
+    const avgWindSpeed = AverageWindSpeed(weatherData)
 
-    for (let j = 0; j < types.length; j++) {
-        for (let i = historicalMeasurements.length - 1; i >= 0; i--) {
-            if (historicalMeasurements[i].getType() === types[j]) {
-                latestMeasurements.push(historicalMeasurements[i]);
-                break;
-            }
-        }
-    }
     return {
-        historicalMeasurements, forecastMeasurements, latestMeasurements
+        historicalMeasurements, forecastMeasurements, latestMeasurements,
+        minTemperature, maxTemperature, totalPrecipitation, avgWindSpeed
     }
 }
 export default model
 
-export function MinTemperature(weatherData) {
-    let min = weatherData[0].getValue()
+function LatestMeasurements(historicalMeasurements) {
+    let latestMeasurements = []
+    let types = WEATHER_TYPES
+
+    for (let j = 0; j < types.length; j++) {
+        for (let i = historicalMeasurements.length - 1; i >= 0; i--) {
+            if (historicalMeasurements[i].getType() === types[j]) {
+                latestMeasurements.push(historicalMeasurements[i])
+                break
+            }
+        }
+    }
+    return latestMeasurements
+}
+
+function MinTemperature(weatherData) {
+    let min = weatherData[0].value
     let lastDay = FindLastDay(weatherData)
     for (let i = 0; i < weatherData.length; i++) {
-        if (weatherData[i].getType() === WEATHER_TYPES[0] && weatherData[i].getTime() === lastDay) {
-            min = Math.min(min, weatherData[i].getValue())
+        if (weatherData[i].type === WEATHER_TYPES[0] && weatherData[i].time === lastDay) {
+            min = Math.min(min, weatherData[i].value)
         }
     }
     return min
 }
 
-export function MaxTemperature(weatherData) {
-    let max = weatherData[0].getValue()
+function MaxTemperature(weatherData) {
+    let max = weatherData[0].value
     let lastDay = FindLastDay(weatherData)
     for (let i = 0; i < weatherData.length; i++) {
-        if (weatherData[i].getType === WEATHER_TYPES[0] && weatherData[i].getTime() === lastDay) {
-            max = Math.max(max, weatherData[i].getValue())
+        if (weatherData[i].type === WEATHER_TYPES[0] && weatherData.time === lastDay) {
+            max = Math.max(max, weatherData[i].value)
         }
     }
     return max
 }
 
-export function TotalPrecipitation(weatherData) {
+function TotalPrecipitation(weatherData) {
     let totalPrecipitation = 0
     let lastDay = FindLastDay(weatherData)
     for (let i = 0; i < weatherData.length; i++) {
-        if (weatherData[i].getType === WEATHER_TYPES[1] && weatherData[i].getTime() === lastDay) {
-            totalPrecipitation += weatherData[i].getValue()
+        if (weatherData[i].type === WEATHER_TYPES[1] && weatherData[i].time === lastDay) {
+            totalPrecipitation += weatherData[i].value
         }
     }
     return Math.round(totalPrecipitation * 100) / 100
 }
 
-export function AverageWindSpeed(weatherData) {
+function AverageWindSpeed(weatherData) {
     let sum = 0
     let count = 0
     let lastDay = FindLastDay(weatherData)
     for (let i = 0; i < weatherData.length; i++) {
-        if (weatherData[i].getType === WEATHER_TYPES[3] && weatherData[i].getTime() === lastDay) {
-            sum += weatherData[i].getValue()
+        if (weatherData[i].type === WEATHER_TYPES[3] && weatherData[i].time === lastDay) {
+            sum += weatherData[i].value
             count++
         }
     }
@@ -107,8 +118,8 @@ export function AverageWindSpeed(weatherData) {
 function FindLastDay(weatherData) {
     let lastDay = null
     for (let i = 0; i < weatherData.length; i++) {
-        if (!lastDay || weatherData[i].getTime() > lastDay) {
-            lastDay = weatherData[i].getTime()
+        if (!lastDay || weatherData[i].time > lastDay) {
+            lastDay = weatherData[i].time
         }
     }
     return lastDay
