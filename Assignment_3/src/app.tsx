c77e5a64c8f1aa64adfac5c908dcda93d66485da
GeorgianaIon <ion.georgiana@hotmail.com>
2023-10-13 15:44:41 +0300

    added style and css loader

--- Assignment_3/src/app.tsx
+++ Assignment_3/src/app.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import AppRouter from "./AppRouter";
+import "./app.css";
+
 function App() {
   return (
     <>
--- Assignment_3/webpack.config.js
+++ Assignment_3/webpack.config.js
@@ -24,6 +24,10 @@ test: /\.tsx?$/,
 use: 'ts-loader',
 exclude: /node_modules/,
 },
+{
+test: /\.css?$/,
+use: ["style-loader", "css-loader"],
+},
 ]
 },
 resolve: {
