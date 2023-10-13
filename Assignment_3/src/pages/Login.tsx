2db02410c1ba1a6e7b2fe5e04e5eea0f56010a44
GeorgianaIon <ion.georgiana@hotmail.com>
2023-10-13 16:27:42 +0300

    login page

--- Assignment_3/src/pages/Login.tsx
+++ Assignment_3/src/pages/Login.tsx
@@ -1,44 +1,52 @@
-import React from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 const LoginPage = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
   };
 
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
+    <div className="login-container">
+      <h1 className="login-title">Login</h1>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="login-form-group">
+          <label className="login-form-label" htmlFor="username">
+            Username:
+          </label>
           <input
+            className="login-form-input"
             type="text"
             id="username"
             value={username}
             onChange={handleUsernameChange}
           />
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
+        <div className="login-form-group">
+          <label className="login-form-label" htmlFor="password">
+            Password:
+          </label>
           <input
+            className="login-form-input"
             type="password"
             id="password"
             value={password}
             onChange={handlePasswordChange}
           />
         </div>
-        <button type="submit">Login</button>
+        <button className="login-form-button" type="submit">
+          Login
+        </button>
       </form>
     </div>
   );
