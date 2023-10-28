289504da627928febe79702ba1eb1a0ade1f2225
GeorgianaIon <ion.georgiana@hotmail.com>
2023-10-28 10:32:58 +0200

    added font, board with radius
    app => App

--- Assignment_3/src/main.tsx
+++ Assignment_3/src/main.tsx
@@ -1,14 +1,14 @@
-import { store } from './config/store';
-import { Provider } from 'react-redux';
-import App from './app';
-import ReactDOM from 'react-dom';
-import React from 'react';
+import { store } from "./config/store";
+import { Provider } from "react-redux";
+import App from "./App";
+import ReactDOM from "react-dom";
+import React from "react";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
