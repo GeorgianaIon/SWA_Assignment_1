5f312dcf02c4a5832902b4580858232ea6323e7a
GeorgianaIon <ion.georgiana@hotmail.com>
2023-11-12 13:25:48 +0100

    non-functional register

---
+++ Assignment_4/src/pages/Register.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+      <h1>Register</h1>
+      <Form @on-submit="handleSubmit" :isRegister="true" />
+    </div>
+  </template>
+  
+  <script lang="ts">
+  interface Credentials {
+  username: string;
+  password: string;
+}
+
+  import { defineComponent } from 'vue';
+  import Form from '../components/Form.vue'; 
+  
+  export default defineComponent({
+    components: {
+      Form,
+    },
+    methods: {
+      async handleSubmit(credentials: Credentials) {
+        console.log(credentials);
+        this.$router.push('/');
+      },
+    },
+  });
+  </script>
+  
+  <style scoped>
+
+  </style>
+  
--- Assignment_4/src/router/router.ts
+++ Assignment_4/src/router/router.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-    { path: '/', component: () => import("../pages/Login.vue"), name: 'home' },
+    { path: '/', component: () => import("../pages/Login.vue"), name: 'login' },
+    { path: '/register', component: () => import("../pages/Register.vue"), name: 'register' },
   ]
 
   const router = createRouter({
